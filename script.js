6d7e6b081b1c24c1afe8b60fd3ab30f5842b3137
Ayodeji Oni <88333351+Ayo-Oni-515@users.noreply.github.com>
2025-05-16 08:51:40 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+let eventButton = document.getElementById('event-button');
+let eventParagraph = document.getElementById('event-paragraph');
 
+
+eventButton.addEventListener('click', () => {
+    if (eventParagraph.style.fontSize == '1rem'){
+        eventButton.textContent = 'Reset';
+        eventParagraph.style.fontSize = '3rem';
+    } else {
+        eventButton.textContent = 'Click Me';
+        eventParagraph.style.fontSize = '1rem';
+    }
+})
